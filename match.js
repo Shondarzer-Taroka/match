e66cc4883599cf91eff65996d5e778f5d0330d99
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-08-09 19:33:05 +0600

    match

---
+++ match.js
@@ -0,0 +1,25 @@
+
+let text = "The rain in SPAIN stays mainly in the plain";
+// let match= text.match("ain");
+// let match= text.match(/ain/);
+// let match= text.match(/n/g);
+let match= text.match(/n/gi);
+
+//  for (const m of match) {
+//     console.log(m);
+//  }
+
+//   for (const key in match) {
+//      console.log(key);
+//   }
+ 
+
+
+//  console.log(typeof match);
+
+
+
+
+
+
+console.log(match);
